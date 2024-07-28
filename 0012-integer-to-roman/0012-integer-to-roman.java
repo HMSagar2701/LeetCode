de66f390738d5b9539351c35e8e17b0b9d891cb8
H M Sagar <sagarhm2701@gmail.com>
2024-07-28 22:16:31 +0530

    Time: 13 ms (7.23%), Space: 44.9 MB (12.20%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String intToRoman(int num) {
+        String [] ones = {"", "I","II","III","IV","V","VI","VII","VIII","IX"};
+        String[] tens = {"","X", "XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String[] hndrs ={ "","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String[] thous = {"","M","MM","MMM"};
+
+        return thous[num/1000]+hndrs[(num%1000)/100]+tens[(num%100)/10]+ones[num%10];
+    }
+}
